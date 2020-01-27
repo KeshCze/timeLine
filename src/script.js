2fd734b09e7f4b0f561f7a6347f8b8f0106b9b40
Lukáš Babinec <lukas.kesh@gmail.com>
2020-01-27 20:18:17 +0100

    scrollalbe bottom line

--- src/script.js
+++ src/script.js
@@ -4,3 +4,19 @@ let data = [
     {Action : 411, TimeStamp : "January 27, 2020 13:00:00", Pernr : "89065325", Name : "Lukáš Babinec"},
     {Action : 413, TimeStamp : "January 27, 2020 14:00:00", Pernr : "89065325", Name : "Lukáš Babinec"},
 ];
+
+let timeLine = {
+    daysRendered: 7,
+    hoursOnScreen: 8,
+    render(){
+        for (let index = 0; index < this.daysRendered*24; index++) {
+            let element = document.createElement('div');
+            document.querySelector('.timeLine-tail').appendChild(element);
+            
+        }
+    }
+}
+
+let root = document.documentElement;
+root.style.setProperty('--hours-on-screen',timeLine.daysRendered);
+timeLine.render();
