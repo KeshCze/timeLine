c5f1018fcc9f73a93b25ee2681d4b71225994ecc
Lukáš Babinec <lukas.kesh@gmail.com>
2020-01-27 18:43:48 +0100

    first commit

---
+++ src/script.js
@@ -0,0 +1,6 @@
+let data = [
+    {Action : 411, TimeStamp : "January 27, 2020 11:00:00", Pernr : "89065325", Name : "Lukáš Babinec"},
+    {Action : 413, TimeStamp : "January 27, 2020 12:00:00", Pernr : "89065325", Name : "Lukáš Babinec"},
+    {Action : 411, TimeStamp : "January 27, 2020 13:00:00", Pernr : "89065325", Name : "Lukáš Babinec"},
+    {Action : 413, TimeStamp : "January 27, 2020 14:00:00", Pernr : "89065325", Name : "Lukáš Babinec"},
+];
