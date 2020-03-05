324f2a944a1271ad293ab0cd3dea0e17d47310e4
z003yexj <lukas.babinec@siemens.com>
2020-03-05 12:48:22 +0100

    templated row to render for independent user

--- src/script.js
+++ src/script.js
@@ -1,5 +1,10 @@
+let container = document.querySelector('body');
+let dataPeople =[
+    {Pernr : "89065325", Name : "Lukáš Babinec", lastScroll : 0},
+];
 
-let data = [
+
+let dummyPersonData = [
     {Action : 301, ValidFrom : "January 27, 2020 11:00:00", ValidTo : "January 27, 2020 18:00:00", Pernr : "89065325", Name : "Lukáš Babinec"},
     {Action : 500, ValidFrom : "January 27, 2020 11:00:00", ValidTo : "January 27, 2020 11:30:00", Pernr : "89065325", Name : "Lukáš Babinec"},
     {Action : 500, ValidFrom : "January 27, 2020 14:00:00", ValidTo : "January 27, 2020 14:30:00", Pernr : "89065325", Name : "Lukáš Babinec"},
@@ -11,18 +16,33 @@ let data = [
         
 ];
 
+
 let timeLine = {
     daysRendered: 7,
-    hoursOnScreen: 7,
-    render(){ 
+    hoursOnScreen: 4,
+    minWidthToDisplayOrderNumber: 100,
+    renderRows(){
+        dataPeople.forEach((d) => {
+            // Render row            
+            let rowString = this.returnRow(d);
+            let wrap = document.createElement('div');
+            wrap.innerHTML = rowString;
+            container.appendChild(wrap);
+
+            // Render timeline for row
+            this.renderTimeline(d.Pernr,dummyPersonData);
+        });
+
+    },
+    renderTimeline(pernr,data){ 
         // ================================
         // Reset before render
         // ================================ 
-        document.querySelector('.tail').innerHTML = "";
-        document.querySelector('.pos-0').innerHTML = "<hr>";
-        document.querySelector('.pos-1').innerHTML = "<hr>";
-        document.querySelector('.pause').innerHTML = "<hr>";
-        document.querySelector('.login').innerHTML = "<hr>";
+        document.querySelector(`.row[data-pernr="${pernr}"] .tail`).innerHTML = "";
+        document.querySelector(`.row[data-pernr="${pernr}"] .pos-0`).innerHTML = "<hr>";
+        document.querySelector(`.row[data-pernr="${pernr}"] .pos-1`).innerHTML = "<hr>";
+        document.querySelector(`.row[data-pernr="${pernr}"] .pause`).innerHTML = "<hr>";
+        document.querySelector(`.row[data-pernr="${pernr}"] .login`).innerHTML = "<hr>";
 
 
         // ================================
@@ -38,7 +58,7 @@ let timeLine = {
                 element.appendChild(hr);
             }    
             element.dataset.content = index % 24;
-            document.querySelector('.tail').appendChild(element);
+            document.querySelector(`.row[data-pernr="${pernr}"] .tail`).appendChild(element);
         }
         
         // ================================
@@ -46,7 +66,7 @@ let timeLine = {
         // ================================ 
         data.forEach((el) => {
             // Size of one second on a timeline in Px
-            let sizeOfOneSecOnTimeLine = ((document.querySelector('.tail div').getBoundingClientRect().width / 60) / 60);
+            let sizeOfOneSecOnTimeLine = ((document.querySelector(`.row[data-pernr="${pernr}"] .tail div`).getBoundingClientRect().width / 60) / 60);
 
 
             // Diference in seconds between start of the timeline and event action
@@ -62,25 +82,91 @@ let timeLine = {
             // Determine to which line to put the action log
             switch (el.Action) {
                 case 301: 
-                    document.querySelector('.login').appendChild(element);                                
+                    document.querySelector(`.row[data-pernr="${pernr}"] .login`).appendChild(element);                                
                     break;
                 case 304:
                 case 500: 
-                    document.querySelector('.pause').appendChild(element);                                
+                    document.querySelector(`.row[data-pernr="${pernr}"] .pause`).appendChild(element);                                
                     break;
                 case 402:
+                    // Add order number text
+                    let OrderNumberText = document.createElement('div');
+                    OrderNumberText.innerText = `${el.OrderNumber} / ${('0' + el.Piece).slice(-2)}`;
+                    // If action element is too small hide the order number
+                    if(actionTimespan*sizeOfOneSecOnTimeLine < this.minWidthToDisplayOrderNumber)
+                    {
+                        OrderNumberText.style.setProperty('display','none');
+                    }
+                    element.appendChild(OrderNumberText);
+
                     if(el.Index === 0){
-                        document.querySelector('.pos-0').appendChild(element);
+                        document.querySelector(`.row[data-pernr="${pernr}"] .pos-0`).appendChild(element);
                     }
                     else{
-                        document.querySelector('.pos-1').appendChild(element);
+                        document.querySelector(`.row[data-pernr="${pernr}"] .pos-1`).appendChild(element);
                     }
                     break;
                 default:
-                    console.log("Unknown action",el);
+                    console.error("Unknown action",el);
                     break;
             }
         });
+
+        /// Scroll to the center of the timeline
+        document.querySelector(`.row[data-pernr="${pernr}"] .head`).scroll((document.querySelector(`.row[data-pernr="${pernr}"] .head`).scrollWidth/2.0),0);
+
+        // Add event to calculate pointer data
+        document.querySelector(`.row[data-pernr="${pernr}"] .head`).addEventListener('scroll',(e) => {
+            let el = e.target;
+
+            // Size of one second on a timeline in Px            
+            let sizeOfOneSecOnTimeLine = (60.0 / (document.querySelector(`.row[data-pernr="${pernr}"] .tail div`).getBoundingClientRect().width));
+            // Time offset defined by scroll
+            let movedMinutes = (el.scrollLeft * sizeOfOneSecOnTimeLine)*60;
+            // Add moved time offset to the begining of the redered time        
+            let scrolledDate = new Date((TimelineBeginingDate.getTime()) + movedMinutes *1000);
+            // Override new timestamp
+            document.querySelector(`.row[data-pernr="${pernr}"] .pointer-text`).innerText = `${scrolledDate.getDay()}.${scrolledDate.getMonth()} ${scrolledDate.getHours()}:${scrolledDate.getMinutes()}`;
+        });
+    },
+    returnRow(data){
+        return `
+        <div class="row" data-pernr="${data.Pernr}">
+            <h1>${data.Name}</h1>
+            <div class="timeLine">
+                <hr class="pointer">
+                <div class="pointer-text">23.3. 14:53</div>
+                <div class="arrow-left">
+                    <svg >
+                        <use class="arrow" xlink:href="src/styles/img/arr-left.svg#arr-left"></use>
+                    </svg>
+                </div>
+                <div class="arrow-right">
+                    <svg >
+                        <use class="arrow" xlink:href="src/styles/img/arr-right.svg#arr-right"></use>
+                    </svg>
+                </div>
+                <div class="head dragscroll">
+                    <div class="lines">
+                        <div class="line-wrap pos-0">
+                            <hr>
+                        </div>
+                        <div class="line-wrap pos-1">
+                            <hr>                        
+                        </div>
+                        <div class="line-wrap pause">
+                            <hr>                        
+                        </div>
+                        <div class="line-wrap login">
+                            <hr>                        
+                        </div>
+                    </div>
+                    <div class="tail">
+                    </div>
+                </div>            
+            </div>            
+        </div>
+        `
     }
 }
 
@@ -94,34 +180,32 @@ document.documentElement.style.setProperty('--hours-on-screen',timeLine.hoursOnS
 document.documentElement.style.setProperty('--daysRendered',timeLine.daysRendered);
 
 // Run render
-timeLine.render();
+timeLine.renderRows();
 
-/// Scroll to the center of the timeline
-document.querySelector('.head').scroll((document.querySelector('.head').scrollWidth/2.0),0);
+// // Set arrow scrolling events
+// let lastScroll;
+// document.querySelectorAll('.dragscroll').addEventListener('scroll', (e) => {
+//     let el = e.target;
+//     if(el.scrollLeft < lastScroll) {
+//         //scroll left
+//         document.querySelector('.arrow-left use').classList.toggle('arrow-light',true);
+//         document.querySelector('.arrow-right use').classList.toggle('arrow-light',false);
+//     } else {
+//         //scroll right        
+//         document.querySelector('.arrow-left use').classList.toggle('arrow-light',false);
+//         document.querySelector('.arrow-right use').classList.toggle('arrow-light',true);
 
-// Set arrow scrolling events
-let lastScroll;
-document.querySelector('.dragscroll').addEventListener('scroll', (e) => {
-    let el = document.querySelector('.dragscroll');
-    if(el.scrollLeft < lastScroll) {
-        //scroll left
-        document.querySelector('.arrow-left use').classList.toggle('arrow-light',true);
-        document.querySelector('.arrow-right use').classList.toggle('arrow-light',false);
-    } else {
-        //scroll right        
-        document.querySelector('.arrow-left use').classList.toggle('arrow-light',false);
-        document.querySelector('.arrow-right use').classList.toggle('arrow-light',true);
-
-    }
-    setTimeout(() => {
-        document.querySelector('.arrow-right use').classList.toggle('arrow-light',false);
-        document.querySelector('.arrow-left use').classList.toggle('arrow-light',false);
-    },200);
-    lastScroll = el.scrollLeft;
-  });
+//     }
+//     // reset highlight
+//     setTimeout(() => {
+//         document.querySelector('.arrow-right use').classList.toggle('arrow-light',false);
+//         document.querySelector('.arrow-left use').classList.toggle('arrow-light',false);
+//     },200);
+//     lastScroll = el.scrollLeft;
+//   });
 
 // On resize re-render event
 window.addEventListener("resize",() => {
-    timeLine.render();
+    timeLine.renderRows();
     console.log("resize");
 });
